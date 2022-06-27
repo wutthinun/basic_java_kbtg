e14ee83520f3b6663b6470850fd0b5a47f74b335
wutthinan <outsource0100@set.or.th>
2022-06-27 19:58:56 +0700

    Feat : Add var int

---
+++ 01_primitive/Int.java
@@ -0,0 +1,13 @@
+public class Int {
+	public static void main(String[] args) {
+		int b10 = 101; // base 10
+		int _b16 = 01426; // base 16
+		int $b8 = 0x123; // base 8
+		int bi = 0b110;
+
+		System.out.println(b10);
+		System.out.println(_b16);
+		System.out.println($b8);
+		System.out.println(bi);
+	}
+}
