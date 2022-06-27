fb9c8b64eecad5b0851d4acba10c2066ad8795fc
wutthinan <outsource0100@set.or.th>
2022-06-27 20:09:43 +0700

    Feat : Add floating point ex

---
+++ 01_primitive/FloatingPoint.java
@@ -0,0 +1,9 @@
+public class FloatingPoint {
+	public static void main(String[] args) {
+		float standard = 583.45f; // (standard)
+		float scientific = 5.8345e2f; // (scientific)
+
+		System.out.println(standard);
+		System.out.println(scientific);
+	}
+}
