599775f568018a09d3c13756a9acf0b40788db24
wutthinan <outsource0100@set.or.th>
2022-06-27 20:26:16 +0700

    Feat : Add char ex

---
+++ 01_primitive/MainChar.java
@@ -0,0 +1,14 @@
+public class MainChar {
+
+	public static void main(String[] args) {
+		char ch0 = 'a';
+		char ch1 = '\u0061';
+		char ch2 = '\n';
+		char ch3 = '\"';
+
+		System.out.println(ch0);
+		System.out.println(ch1);
+		System.out.println(ch2);
+		System.out.println(ch3);
+	}
+}
