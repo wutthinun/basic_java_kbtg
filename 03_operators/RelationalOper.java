0548213c69df24858cc2177e38150dd7e2fb1d9e
wutthinan <outsource0100@set.or.th>
2022-06-28 11:39:16 +0700

    Add Relational Oper

---
+++ 03_operators/RelationalOper.java
@@ -0,0 +1,24 @@
+public class RelationalOper {
+	public static void main(String[] args) {
+		int i = 37;
+		int j = 42;
+		int k = 42;
+
+		System.out.println("0" + (i > j));
+		System.out.println("0" + (j > i));
+		System.out.println("0" + (k > j));
+		System.out.println("0" + (i >= j));
+		System.out.println("0" + (j >= i));
+		System.out.println("0" + (k >= j));
+		System.out.println("0" + (i < j));
+		System.out.println("0" + (j < i));
+		System.out.println("0" + (k < j));
+		System.out.println("0" + (i <= j));
+		System.out.println("0" + (j <= i));
+		System.out.println("0" + (k <= j));
+		System.out.println("0" + (i == j));
+		System.out.println("0" + (k == j));
+		System.out.println("0" + (i != j));
+		System.out.println("0" + (k != j));
+	}
+}
