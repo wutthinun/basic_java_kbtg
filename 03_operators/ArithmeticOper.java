53a6621ec420b5e81371eaedcedcc606a4198302
wutthinan <outsource0100@set.or.th>
2022-06-28 11:34:03 +0700

    Add arithmetic operator

---
+++ 03_operators/ArithmeticOper.java
@@ -0,0 +1,20 @@
+public class ArithmeticOper {
+	public static void main(String[] args) {
+		int i = 37;
+		int j = 42;
+		double x = 27.475d;
+		float y = 7.22f;
+
+		System.out.println("0: " + (i + j));
+		System.out.println("1: " + (x + y));
+		System.out.println("2: " + (i - j));
+		System.out.println("3: " + (x - y));
+		System.out.println("4: " + (i * j));
+		System.out.println("5: " + (x * y));
+		System.out.println("6: " + (i / j));
+		System.out.println("7: " + (x / y));
+		System.out.println("8: " + (i % j));
+		System.out.println("9: " + (x % y));
+		System.out.println("10: " + (i * x));
+	}
+}
