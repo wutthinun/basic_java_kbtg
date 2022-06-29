d430b52a035301d79341081ba3b5665e5b441957
wutthinan <outsource0100@set.or.th>
2022-06-29 09:41:46 +0700

    Add : wrapper class ex

---
+++ 08_wraper_type/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+	public static void main(String[] args) {
+		String s = "This is a String";
+		Integer integer = 1; // Wrapper class of int
+		Long long1 = 100L; // Wrapper class of long
+		Float float1 = 1.11f; // Wrapper class of float
+		Double double1 = 2.22d; // Wrapper class of double
+		Boolean boolean1 = true; // Wrapper class of boolean
+	}
+}
